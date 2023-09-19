453e629ab66db31a390377f0dd5bb4c0d5fc1e7d
MariasRochas <132393830+MariasRochas@users.noreply.github.com>
2023-09-19 15:37:29 -0300

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAs9uTkZ7bm2vJ7_X7I-DB4i6c18IhSH0M",
+    authDomain: "p-c53e2.firebaseapp.com",
+    databaseURL: "https://p-c53e2-default-rtdb.firebaseio.com",
+    projectId: "p-c53e2",
+    storageBucket: "p-c53e2.appspot.com",
+    messagingSenderId: "641404643536",
+    appId: "1:641404643536:web:a774318cd87c2a374d56d5"
+  };
